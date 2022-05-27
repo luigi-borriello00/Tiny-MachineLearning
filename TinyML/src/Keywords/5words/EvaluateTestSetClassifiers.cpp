d9acdcdfa89b93f2332a59a63ada50affc47e6f9
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2022-05-27 14:23:53 +0200

    fix notebook evaluations

--- TinyML/src/Keywords/5words/EvaluateTestSetClassifiers.cpp
+++ TinyML/src/Keywords/5words/EvaluateTestSetClassifiers.cpp
@@ -5,9 +5,9 @@
 /* Uncomment the model that yout want to test */
 //#include "Models/RF.h" // model file
 //#include "Models/CART.h"            // model file
-//#include "Models/SVC.h"            // model file
+#include "Models/SVC.h"            // model file
 //#include "Models/LR.h"           // model file
-#include "Models/GNB.h"            // model file
+//#include "Models/GNB.h"            // model file
 #define NUMBER_OF_LABELS 5                            // number of voice labels
 const String words[NUMBER_OF_LABELS] = {"No", "Yes", "Ok", "Start", "Stop"};
 #define FEATURE_SIZE 32
@@ -17,8 +17,8 @@ const String words[NUMBER_OF_LABELS] = {"No", "Yes", "Ok", "Start", "Stop"};
 // Eloquent::ML::Port::RandomForest model;
 // Eloquent::ML::Port::DecisionTree model;
 // Eloquent::ML::Port::LogisticRegression model;
-// Eloquent::ML::Port::SVM model;
- Eloquent::ML::Port::GaussianNB model;
+ Eloquent::ML::Port::SVM model;
+// Eloquent::ML::Port::GaussianNB model;
 
 TestSet testSet;
 int prediction[TEST_SIZE];
